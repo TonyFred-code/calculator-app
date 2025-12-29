8adbdc5d57827d2764bdbf763c0663b5f0024e82
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-29 08:46:10 +0100

    feat: create basic calculator layout
    add digit input logic
    add decimal point input logic
    add character deletion logic
    add calculator state reset logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,191 @@
-export default function App() {}
+import { useState } from "react";
+import {
+  BUTTON_KEY_MAPPING,
+  CALCULATION_ERROR,
+  DECIMAL_POINT,
+  INITIAL_DISPLAY,
+  MAX_DISPLAY_CHAR_LENGTH,
+} from "./constants/calculator.js";
+
+export default function App() {
+  const [firstOperand, setFirstOperand] = useState(0);
+  const [displayedValue, setDisplayedValue] = useState(INITIAL_DISPLAY);
+  const [secondOperand, setSecondOperand] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [waitingForSecondOperand, setWaitingForSecondOperand] = useState(false);
+
+  function resetCalculator() {
+    setDisplayedValue("0");
+    setFirstOperand(null);
+    setSecondOperand(null);
+    setOperator(null);
+  }
+
+  function inputDigit(digit) {
+    if (displayedValue.length >= MAX_DISPLAY_CHAR_LENGTH) {
+      alert("Maximum display characters length reached");
+      return;
+    }
+
+    if (displayedValue === "MATH ERROR") {
+      resetCalculator();
+      setDisplayedValue(`${digit}`);
+    } else if (displayedValue === "0") {
+      setDisplayedValue(`${digit}`);
+    } else if (displayedValue === "-") {
+      setDisplayedValue(`-${digit}`);
+    } else if (Number(displayedValue) === firstOperand && operator !== null) {
+      setDisplayedValue(`${digit}`);
+    } else {
+      setDisplayedValue(`${displayedValue}${digit}`);
+    }
+
+    if (operator && firstOperand !== null) {
+      setSecondOperand(Number(displayedValue));
+    } else {
+      setFirstOperand(Number(displayedValue));
+    }
+  }
+
+  function handleDel() {
+    if (displayedValue === INITIAL_DISPLAY) return;
+
+    if (displayedValue === CALCULATION_ERROR) {
+      resetCalculator();
+      return;
+    }
+
+    const updatedDisplayValue = `${displayedValue.slice(0, -1)}`;
+
+    if (displayedValue === "") {
+      setDisplayedValue(INITIAL_DISPLAY);
+    } else {
+      setDisplayedValue(updatedDisplayValue);
+    }
+
+    if (secondOperand === null) {
+      setFirstOperand(Number(updatedDisplayValue));
+    } else if (operator && firstOperand !== null) {
+      setSecondOperand(Number(displayedValue));
+    }
+  }
+
+  function inputDecimal() {
+    if (displayedValue.length >= MAX_DISPLAY_CHAR_LENGTH) {
+      alert("Maximum display characters length reached");
+      return;
+    }
+
+    if (displayedValue === "MATH ERROR") {
+      resetCalculator();
+    }
+
+    if (waitingForSecondOperand && secondOperand === null) {
+      setWaitingForSecondOperand(false);
+      setDisplayedValue(INITIAL_DISPLAY);
+      return;
+    }
+
+    if (!displayedValue.includes(DECIMAL_POINT)) {
+      setDisplayedValue(`${displayedValue}${DECIMAL_POINT}`);
+    }
+  }
+
+  function handleButtonClick(type, value) {
+    console.log(type, value);
+
+    switch (value) {
+      case "+":
+      case "-":
+      case "/":
+      case "*":
+        break;
+      case "=":
+        break;
+      case ".":
+        inputDecimal(value);
+        break;
+      case "RESET":
+        resetCalculator();
+        break;
+      case "DEL":
+        handleDel();
+        break;
+      default:
+        if (Number.isInteger(parseFloat(value))) {
+          inputDigit(value);
+        }
+    }
+  }
+
+  function formatDisplay(value) {
+    if (value === INITIAL_DISPLAY) return value;
+
+    const [int, dec] = value.split(".");
+    const formattedInt = new Intl.NumberFormat("en-US").format(int);
+
+    return dec !== undefined ? `${formattedInt}.${dec}` : `${formattedInt}`;
+  }
+
+  return (
+    <div className="min-h-screen bg-main-bg px-5 py-7 flex items-center justify-center text-[32px] text-white font-bold">
+      <div className="w-11/12 max-w-sm space-y-7 select-none">
+        {/* HEADER */}
+        <div className="flex justify-between">
+          <h1 className="text-3xl">calc</h1>
+          {/* <div className="flex gap-8 items-end">
+            <p className="text-sm font-bold uppercase">theme</p>
+            <div className="flex flex-col gap-1 relative">
+              <div className="text-sm flex justify-around -top-6 left-[10%] right-[10%] absolute  ">
+                {themes.map((theme) => {
+                  return <span key={theme.code}>{theme.code}</span>;
+                })}
+              </div>
+              <div className="w-20 rounded-full bg-screen-bg p-1.5 flex justify-evenly">
+                {themes.map((theme) => {
+                  return (
+                    <button className="rounded-full size-5 block bg-key-red-toggle-bg cursor-pointer"></button>
+                  ); })}
+              </div>
+            </div>
+          </div> */}
+          {/* TODO: theme */}
+        </div>
+
+        {/* SCREEN */}
+        <form className="w-full">
+          <label htmlFor="operands" className="sr-only">
+            Operands:
+          </label>
+          <input
+            type="text"
+            name="operands"
+            value={formatDisplay(displayedValue)}
+            disabled={true}
+            id="operands"
+            className="pointer-events-none p-4 bg-screen-bg text-white w-full text-end rounded-lg"
+          />
+        </form>
+
+        {/* KEYPAD */}
+        <div className="keypad bg-keypad-bg grid gap-3 p-5 rounded-xl">
+          {BUTTON_KEY_MAPPING.map((buttonKey) => {
+            const { value, type, area } = buttonKey;
+
+            return (
+              <button
+                key={value}
+                className={`${type}-type grid-area-${String(area).toLowerCase()}`}
+                onClick={() => {
+                  handleButtonClick(type, value);
+                }}
+              >
+                {value}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
